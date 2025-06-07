f1ddd72bb5fa014a22a9be900badf66500f00ccc
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-07 17:31:30 -0400

    Add user resolver for fetching individual user by ID
    - Implemented user resolver to retrieve a user based on userId
    - Enhances the GraphQL API by allowing specific user queries

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -2,8 +2,14 @@ import { users } from '../data/data.js'
 
 const UserResolver = {
   Query: {
+    // users resolver
     users() {
       return users
+    },
+
+    // user(userId: ID!) resolver
+    user(_, { userId }) {
+      return users.find((u) => u._id === userId)
     },
   },
   Mutation: {},
