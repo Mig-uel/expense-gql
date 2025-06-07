fd710273e972c415e376c390dacf2933923bcdf8
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-07 17:46:49 -0400

    Refactor server setup to use HTTP server with Apollo
    - Integrate HTTP server for Apollo Server to handle requests
    - Replace standalone server setup with express middleware
    - Ensure compatibility with existing middleware and plugins

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,31 @@
+import cors from 'cors'
+import express from 'express'
+import http from 'http'
+
 import { ApolloServer } from '@apollo/server'
-import { startStandaloneServer } from '@apollo/server/standalone'
+import { expressMiddleware } from '@apollo/server/express4'
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
 import mergedResolvers from './resolvers/index.js'
 import mergedTypeDefs from './resolvers/typeDefs/index.js'
+
+const app = express()
+const httpServer = http.createServer(app)
 
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
   resolvers: mergedResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 })
 
-const { url } = await startStandaloneServer(server)
+await server.start()
 
-console.log(`SERVER READY @ ${url}`)
+app.use(
+  '/',
+  cors(),
+  express.json(),
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ req }),
+  })
+)
+
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve))
