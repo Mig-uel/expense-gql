6faf7ad74ec0bd7c873697b7e8f28bbf811b542a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-08 13:20:57 -0400

    Add dotenv configuration for environment variable management
    - Initialize dotenv to load environment variables
    - Ensures sensitive data is not hardcoded in the application

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,12 @@ import http from 'http'
 import { ApolloServer } from '@apollo/server'
 import { expressMiddleware } from '@apollo/server/express4'
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import { configDotenv } from 'dotenv'
+
 import mergedResolvers from './resolvers/index.js'
 import mergedTypeDefs from './resolvers/typeDefs/index.js'
 
+configDotenv()
 const app = express()
 const httpServer = http.createServer(app)
 
